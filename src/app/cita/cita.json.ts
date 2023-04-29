96dbc5e98cf02a4504e1c0e34420459503e3fdfd
Kevin Gonzalo Cruz Martinez <kevingonzalocruzmartinez@gmail.com>
2023-04-29 13:45:33 -0600

    Se agrego la palabra detalle en el componente cliente y se elimino el archivo cita json al igual el archivo detalle-cita.ts

--- src/app/cita/cita.json.ts
+++
@@ -1,6 +0,0 @@
-import { Cita } from "./cita";
-export const CITAS: Cita []=[
-
-/*  {id: 1, fecha_cita:'2023-01-17', tipo_actividad: 'Mantenimiento', primer_nombre_cliente: 'Angel', segundo_nombre_cliente: 'Luz', primer_nombre_empleado: 'Maria', segundo_nombre_empleado:'Perez'},
-  {id: 2, fecha_cita:'2023-01-17', tipo_actividad: 'Mantenimiento', primer_nombre_cliente: 'Angel', segundo_nombre_cliente: 'Luz', primer_nombre_empleado: 'Maria', segundo_nombre_empleado:'Perez'}*/
-];
